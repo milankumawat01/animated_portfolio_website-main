0a8ed21b26b3dbd17b26d657de992681370aad29
Milan Kumawat <milankumawat01@gmail.com>
2024-01-07 22:22:25 +0530

    no dp

--- src/data/data.js
+++ src/data/data.js
@@ -1,28 +1,25 @@
-
-
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
-    title: "Multimedia Web",
+    img: "https://i.postimg.cc/vHFR2F63/Frame-1.png",
+    title: "Restraunt Website",
     desc: "",
-    link: "",
-    git: "",
+    link: "https://mehakepunjab.ca/",
+    git: "https://www.figma.com/file/NQXQAhOgpHqo3vwpVEvR7x/Mehek-E-Punjab?type=design&node-id=0%3A1&mode=design&t=AwTZXz0xnMfVf8qL-1",
   },
   {
     id: 2,
-    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
-    title: "Tarvel Agency",
-    link: "",
-    git: "",
+    img: "https://i.postimg.cc/76FRR0bf/1-Home-Page-Tenant-Side.png",
+    title: "Rent A Room",
+    link: "https://rentaroom-coderift.vercel.app/",
+    git: "https://github.com/CodeRift/rentaroom",
   },
   {
     id: 3,
-    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
-    title: "Music Player",
+    img: "https://i.postimg.cc/zXKNqX1s/00.png",
+    title: "Miuzik",
     desc: "",
-    link: "",
-    git: "",
-  }
-  
+    link: "https://www.figma.com/community/file/1249076333094763377/miuzik",
+    git: "Not found",
+  },
 ];
